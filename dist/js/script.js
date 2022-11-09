7e395636ec044dc70894cde645c71553a926124d
Koroleva Svetlana <koroleva.4ne@gmail.com>
2022-11-09 15:29:39 +0500

    add changes form in script

--- dist/js/script.js
+++ dist/js/script.js
@@ -103,62 +103,45 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-        // function valideForms(form){
-    //     $(form).validate({
-    //         rules: {
-    //             name: "required",
-    //             phone: "required",
-    //             email: {
-    //                 required: true,
-    //                 email: true
-    //             }
-    //         },
-    //         messages: {
-    //             name: "Пожалуйста, укажите свое имя",
-    //             phone: "Пожалуйста, укажите свой номер телефона",
-    //             email: {
-    //               required: "Пожалуйста, укажите свою почту",
-    //               email: "Неправильно введен адрес почты"
-    //             }
-    //         }
-    //     });
-    // }
-    // valideForms('#communication-form');
+    // Form
 
-    // $('input[name=phone]').mask("+7(999) 999-99-99");
+    function valideForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, укажите свое имя",
+                phone: "Пожалуйста, укажите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, укажите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    }
+    valideForms('#communication-form');
 
-    // $('form').submit(function(e) {
-    //     e.preventDefault(); /* отмена перезагрузки сайта при отправке форм */
-    //     if (!$(this).valid()) { /* чтобы не отправлялась пустая форма */
-    //         return;
-    //     }
-    //     $.ajax({ /* отправка форм на сервер */
-    //         type: "POST", /* получить/отдать(POST) данные */
-    //         url: "mailer/smart.php", /* куда */
-    //         data: $(this).serialize() /* какие данные нужно отправить ($()) и подготовка перед отправкой на сервер serialize() */
-    //     }).done(function() { /* действие после отправки формы */
-    //         $(this).find("input").val(""); /* очистка всех кнопок */
-    //         $('form').trigger('reset'); /* все формы должны очиститься */
-    //     });
-    //     return false;
-    // });
+    $('input[name=phone]').mask("+7(999) 999-99-99");
 
-    // const burgerMenu = document.querySelector('.header__menu');
-    // const burgerBtn = document.querySelector('.hamburger');
-
-    // if(burgerMenu && burgerBtn) {
-    //     burgerBtn.addEventListener('click', function(e) {
-    //         e.preventDefault();
-
-    //         if(burgerBtn.classList.contains('hamburger_active')) {
-    //             burgerBtn.classList.remove('hamburger_active');
-    //             burgerMenu.classList.remove('active');
-    //             document.body.style.overflow = 'auto';
-    //         } else {
-    //             burgerBtn.classList.add('hamburger_active');
-    //             burgerMenu.classList.add('active');
-    //             document.body.style.overflow = 'hidden';
-    //         }
-    //     })
-    // }
+    $('form').submit(function(e) {
+        e.preventDefault(); /* отмена перезагрузки сайта при отправке форм */
+        if (!$(this).valid()) { /* чтобы не отправлялась пустая форма */
+            return;
+        }
+        $.ajax({ /* отправка форм на сервер */
+            type: "POST", /* получить/отдать(POST) данные */
+            url: "mailer/smart.php", /* куда */
+            data: $(this).serialize() /* какие данные нужно отправить ($()) и подготовка перед отправкой на сервер serialize() */
+        }).done(function() { /* действие после отправки формы */
+            $(this).find("input").val(""); /* очистка всех кнопок */
+            $('form').trigger('reset'); /* все формы должны очиститься */
+        });
+        return false;
+    });
 });
